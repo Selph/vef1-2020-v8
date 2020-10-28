ef6e228a759fcffc79a1d91f1bf06342267a265c
Árni Þór Sörensen <arni@hugsnilld.is>
2020-10-28 02:43:07 +0000

    Allt komið

--- scripts.js
+++ scripts.js
@@ -14,7 +14,29 @@
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n, alphabet = '') {
-  return '';
+  let string = str.toLocaleUpperCase();
+  const upprunalegtFylki = alphabet.split('');
+  const hlidradFylki = [];
+  // eslint-disable-next-line no-use-before-define
+  string = sigtun(string, alphabet);
+  const dulkodadFylki = string.split('');
+  for (let i = 0; i < alphabet.length; i += 1) {
+    if (i - n < 0) {
+      hlidradFylki[i + alphabet.length - n] = upprunalegtFylki[i];
+    } else {
+      hlidradFylki[i - n] = upprunalegtFylki[i];
+    }
+  }
+  for (let i = 0; i < dulkodadFylki.length; i += 1) {
+    for (let j = 0; j < alphabet.length; j += 1) {
+      if (dulkodadFylki[i] === upprunalegtFylki[j]) {
+        dulkodadFylki[i] = hlidradFylki[j];
+        break;
+      }
+    }
+  }
+  const dulkodi = dulkodadFylki.join('');
+  return dulkodi;
 }
 
 /**
@@ -26,7 +48,55 @@ function encode(str, n, alphabet = '') {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n, alphabet = '') {
-  return '';
+  let string = str.toLocaleUpperCase();
+  const parsed = parseInt(n, 10);
+  const upprunalegtFylki = alphabet.split('');
+  const hlidradFylki = [];
+  // eslint-disable-next-line no-use-before-define
+  string = sigtun(string, alphabet);
+  const afkodadFylki = string.split('');
+  for (let i = 0; i < alphabet.length; i += 1) {
+    if (i + parsed > alphabet.length - 1) {
+      hlidradFylki[i - alphabet.length + parsed] = upprunalegtFylki[i];
+    } else {
+      hlidradFylki[i + parsed] = upprunalegtFylki[i];
+    }
+  }
+  for (let i = 0; i < afkodadFylki.length; i += 1) {
+    for (let j = 0; j < alphabet.length; j += 1) {
+      if (afkodadFylki[i] === upprunalegtFylki[j]) {
+        afkodadFylki[i] = hlidradFylki[j];
+        break;
+      }
+    }
+  }
+  const afkodi = afkodadFylki.join('');
+  return afkodi;
+}
+
+// Hjálparfall til að sigta út stafi sem eru ekki í lykil
+function sigtun(e, alphabet) {
+  const regex = new RegExp(`[^${alphabet}]`, 'g');
+  const utspyting = e.match(regex);
+  const nyttFylki = [];
+  if (utspyting != null) {
+    for (let i = 0, j = 0; i < e.length - utspyting.length; i += 1, j += 1) {
+      for (let k = 0; k < utspyting.length; k += 1) {
+        if (e[j] === utspyting[k]) {
+          j += 1;
+          for (let l = 0; l < utspyting.length; l += 1) {
+            if (e[j] === utspyting[l]) {
+              j += 1;
+            }
+          }
+        }
+      }
+      nyttFylki[i] = e[j].replaceAll(utspyting, '');
+    }
+    const output = nyttFylki.join('');
+    return output;
+  }
+  return e;
 }
 
 const Caesar = (() => {
@@ -39,8 +109,100 @@ const Caesar = (() => {
   // Default hliðrun, uppfært af "shift"
   let shift = 3;
 
-  function init(el) {
-    // Setja event handlera á viðeigandi element
+  // Strengur
+  let str;
+
+  // Kóði
+  let kodi;
+
+  function init() {
+  // Event listeners
+
+    // Stafrófsgluggi
+    const lyklabox = document.querySelector('#alphabet[type=text]');
+    // eslint-disable-next-line no-use-before-define
+    lyklabox.addEventListener('input', (stafrof));
+
+    // Radio takkar
+    const radio = document.querySelector('.radio');
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of radio.querySelectorAll('label')) {
+      const radioVal = item.querySelector('input');
+      // eslint-disable-next-line no-use-before-define
+      radioVal.addEventListener('click', radioGaga);
+    }
+
+    // Hliðrunarslider
+    const slider = document.querySelector('#shift[type=range]');
+    // eslint-disable-next-line no-use-before-define
+    slider.addEventListener('input', hlidra);
+
+    // Strengur
+    const string = document.querySelector('#input[type=text]');
+    // eslint-disable-next-line no-use-before-define
+    string.addEventListener('input', strengur);
+
+    // Kóði
+    const p = document.createElement('p');
+    p.appendChild(document.createTextNode(''));
+    p.className = 'foo';
+    document.querySelector('.result').appendChild(p);
+
+    // Event handlers
+
+    // Stafrófslykill
+    function stafrof(e) {
+      const lykill = e.target.value;
+      alphabet = lykill;
+      if (type === 'encode') {
+        kodi = encode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      } else if (type === 'decode') {
+        kodi = decode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      }
+    }
+
+    // Radio box
+    function radioGaga(e) {
+      type = e.target.value;
+      if (type === 'encode') {
+        kodi = encode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      } else if (type === 'decode') {
+        kodi = decode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      }
+    }
+
+    // Hliðrun
+    function hlidra(e) {
+      const max = e.target;
+      max.max = alphabet.length;
+      const sliderNumber = document.querySelector('.shiftValue');
+      const hlidrunarnumer = e.target.value;
+      sliderNumber.textContent = hlidrunarnumer;
+      shift = hlidrunarnumer;
+      if (type === 'encode') {
+        kodi = encode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      } else if (type === 'decode') {
+        kodi = decode(str, shift, alphabet);
+        document.querySelector('.foo').textContent = kodi;
+      }
+    }
+  }
+
+  // Strengur
+  function strengur(e) {
+    str = e.target.value;
+    if (type === 'encode') {
+      kodi = encode(str, shift, alphabet);
+      document.querySelector('.foo').textContent = kodi;
+    } else if (type === 'decode') {
+      kodi = decode(str, shift, alphabet);
+      document.querySelector('.foo').textContent = kodi;
+    }
   }
 
   return {
